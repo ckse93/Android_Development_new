c540b1282b1d9b24e8ee6a8c159436108d707889
ckse93 <ckse93@gmail.com>
2019-07-24 16:10:50 -0500

    gravity added on the center linearlayout

--- Dicee/app/src/main/java/com/example/dicee/MainActivity.java
+++ Dicee/app/src/main/java/com/example/dicee/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.dicee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button rollButton = findViewById(R.id.button2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
