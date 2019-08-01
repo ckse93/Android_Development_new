f0c21de57becd7a244faa4b67f47baaa18a33e36
ckse93 <ckse93@gmail.com>
2019-08-01 16:23:44 -0500

    working release

--- Xylophone-Android-master/app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ Xylophone-Android-master/app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
+    private SoundPool msoundpool;
+
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -31,15 +34,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
+        msoundpool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0 );
 
 
         // TODO: Load and get the IDs to identify the sounds
-
+        mCSoundId = msoundpool.load(getApplicationContext(),R.raw.note1_c,1);
+        mDSoundId = msoundpool.load(getApplicationContext(),R.raw.note2_d,1);
+        mESoundId = msoundpool.load(getApplicationContext(),R.raw.note3_e,1);
+        mFSoundId = msoundpool.load(getApplicationContext(),R.raw.note4_f,1);
+        mGSoundId = msoundpool.load(getApplicationContext(),R.raw.note5_g,1);
+        mASoundId = msoundpool.load(getApplicationContext(),R.raw.note6_a,1);
+        mBSoundId = msoundpool.load(getApplicationContext(),R.raw.note7_b,1);
 
     }
 
     // TODO: Add the play methods triggered by the buttons
+    void onClick(View v) {
+        Integer id = v.getId();
+        //Log.i("id : ", id.toString());
+        switch (id) {
+            case R.id.c_key : {
+                Log.println(Log.INFO,"Key pressed :","C");
+                msoundpool.play(mCSoundId,1,1,1,0,1);
+                break;
+            }
+            case R.id.d_key : {
+                Log.println(Log.INFO, "Key pressed :", "D");
+                msoundpool.play(mCSoundId, 1, 1, 1, 0, 1);
+                break;
+            }
+            case R.id.e_key : {
+                Log.println(Log.INFO,"Key pressed :","E");
+                msoundpool.play(mESoundId,1,1,1,0,1);
+                break;
+            }
+            case R.id.f_key : {
+                Log.println(Log.INFO,"Key pressed :","F");
+                msoundpool.play(mFSoundId,1,1,1,0,1);
+                break;
+            }
+            case R.id.g_key : {
+                Log.println(Log.INFO,"Key pressed :","G");
+                msoundpool.play(mGSoundId,1,1,1,0,1);
+                break;
+            }
+            case R.id.a_key : {
+                Log.println(Log.INFO,"Key pressed :","A");
+                msoundpool.play(mASoundId,1,1,1,0,1);
+                break;
+            }
+            case R.id.b_key : {
+                Log.println(Log.INFO,"Key pr essed :","B");
+                msoundpool.play(mBSoundId,1,1,1,0,1);
+                break;
+            }
+            default :
+                break;
+        }
 
-
+    }
 
 }
