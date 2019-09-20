e028fe7a6a4bb07a101ddde648bb339027aa29fc
ckse93 <ckse93@gmail.com>
2019-09-20 16:20:53 -0500

    view controller change works, still need edittext data transfer

--- Clima-Android-master/app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
+++ Clima-Android-master/app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
@@ -1,10 +1,13 @@
 package com.londonappbrewery.climapm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ChangeCityController extends AppCompatActivity {
 
@@ -20,6 +23,16 @@ public class ChangeCityController extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();  // once finish() is called, this view will go away and garbage collector will clean this up.
+            }
+        });
+
+        // this is where the edittext will send back the information to the other view
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                String newCity = editText.getText().toString();
+                Intent mCityIntent = new Intent(ChangeCityController.this, ) // this will navigate back to weatgher
+                return false;
             }
         });
     }
