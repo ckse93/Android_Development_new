2057dc89d96e9b68e12e090e846705a2e224ae9a
ckse93 <ckse93@gmail.com>
2019-09-23 16:29:50 -0500

    data request and receiving works. havent made it to show up to the screen yet

---
+++ bitcoin-ticker-android-ckse93-master/app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -0,0 +1,98 @@
+package com.londonappbrewery.bitcointicker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Constants:
+    // TODO: Create the base URL
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
+
+    // Member Variables:
+    TextView mPriceTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPriceTextView = findViewById(R.id.priceLabel);
+        Spinner spinner = findViewById(R.id.currency_spinner);
+
+        // Create an ArrayAdapter using the String array and a spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, R.layout.spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Bitc", "" + parent.getItemAtPosition(position));
+                Log.d("Bitc", "" + BASE_URL+parent.getItemAtPosition(position));
+                String url = BASE_URL + parent.getItemAtPosition(position);
+                letsDoSomeNetworking(url);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("Bitc", "nothing selected");
+            }
+        });
+
+    }
+
+    // TODO: complete the letsDoSomeNetworking() method
+    private void letsDoSomeNetworking(String url) {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, null,  new JsonHttpResponseHandler() {
+
+            @Override
+            public void onStart(){
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                Log.d("Bitc", "JSON: " + response.toString());
+            }
+
+            @Override
+            public void onRetry(int retryNo){
+
+            }
+        });
+
+
+    }
+
+
+}
